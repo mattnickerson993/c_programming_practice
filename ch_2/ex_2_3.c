24d152ad912e4fd10ddf7338e30797781cec828a
Matt Nickerson <matt.nickerson993@gmail.com>
2023-01-12 08:25:11 -0500

    improve hex

--- ch_2/ex_2_3.c
+++ ch_2/ex_2_3.c
@@ -1,39 +1,60 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
 
 /*
 Goal: write a function htoi(s) which converts a string of hexadecimal digits into its equivalent integer value
-    Method - loop through string and convert to int. Display running total
+    Method: keep running total counting backwards multiply each int (0-f) by 16 ^ n (n counts down to 0)
 
  */
 int htoi(char s[]);
 
 int main(void){
-    // HI 0 a
-    char s[] = "\x48\x49\x30\x61";
-    int res;
+    int res = 0;
+    char s[] = "0X4f5d";
     res = htoi(s);
-    if (res == -1 ){
-        printf("Error: Incorrect input (must be a-z 0-9 A-Z)\n");
+    if (res == -1){
+        printf("Incompatible hex character input\n");
         return -1;
     }
-
-    printf("res: %d\n", res);
+    printf("Result: %d\n", res);
+    return 0;
 }
 
 
 int htoi(char s[]){
-    int i, total;
-    total = 0;
+    int i, n, res;
+    int len = strlen(s);
 
-    for(i = 0; s[i] != '\0'; i++){
-        if((s[i] >= '0' && s[i] <= '9' ) || (s[i] >= 'a' && s[i] <= 'z' || (s[i] >= 'A' && s[i] <= 'Z' ))){
-            // printf("%c\n", (int)(s[i]));
-            total += (int) s[i];
+    //  n is translated to = 0 - 15 ie 0-f in hex.
+    res = 0;
+    while(s[i] != '\0'){
+        if (s[i] == '0' && (s[i + 1] == 'x' || s[i + 1] == 'X')){
+            // skip optional hex chars
+            i += 2;
+            continue;
+        }
+
+        n = tolower(s[i]);
+        if (n >= '0' && n <= '9'){
+            // convert to 0 - 9
+            n -= '0';
+        }
+        else if (n >= 'a' && n <= 'f'){
+            // convert to 10 - 15
+            n = n - 'a' + 10;
         }
         else{
             return -1;
         }
+
+        // RUNNING TOTAL (IE FF = 15 x 16 + 15 x 1 -- COUNTING DOWN)
+        res += n * (int)pow(16, len - i - 1);
+
+        i++;
+
     }
-    return total;
+     return res;
 
 }
